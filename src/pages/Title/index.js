7e6631bd5882a10dbd995f54419940bd1ee8f587
Carlos <carlosrocha@mecajun.com>
2020-11-16 19:36:30 -0300

    title front ok

--- src/pages/Title/index.js
+++ src/pages/Title/index.js
@@ -1,48 +1,52 @@
 import React from 'react'
 import {Link, useLocation} from 'react-router-dom'
+import Background from '../../components/background'
 
 import './styles.css'
 
 function Title(){
     const location = useLocation()
-    const user_object = location.state.params
+    const user_object = location.state!=null?location.state.params:{}
     console.log(JSON.stringify(user_object))
     return(
         <div id="page-title">
-            <div className="logo-container">
-                <svg xmlns="http://www.w3.org/2000/svg" width="268" height="76" viewBox="0 0 268 76">
-                    <text id="BIO" transform="translate(0 62)" fill="#fff" fontSize="57" fontFamily="SegoeUI, Segoe UI"><tspan x="0" y="0">BIO</tspan></text>
-                    <text id="DIVERSIDADE" transform="translate(98 41)" fill="#fff" fontSize="28" fontFamily="SegoeUI, Segoe UI"><tspan x="0" y="0">DIVERSIDADE</tspan></text>
-                    <text id="GENES" transform="translate(98 64)" fill="#fff" fontSize="11" fontFamily="SegoeUI, Segoe UI"><tspan x="0" y="0">GENES</tspan></text>
-                    <text id="ESPÉCIES" transform="translate(140 64)" fill="#fff" fontSize="11" fontFamily="SegoeUI, Segoe UI"><tspan x="0" y="0">ESPÉCIES</tspan></text>
-                    <text id="ECOSSISTEMAS" transform="translate(193 64)" fill="#fff" fontSize="11" fontFamily="SegoeUI, Segoe UI"><tspan x="0" y="0">ECOSSISTEMAS</tspan></text>
-                    <circle id="Elipse_1" data-name="Elipse 1" cx="1.5" cy="1.5" r="1.5" transform="translate(134 59)" fill="#fff"/>
-                    <circle id="Elipse_2" data-name="Elipse 2" cx="1.5" cy="1.5" r="1.5" transform="translate(187 59)" fill="#fff"/>
-                </svg>
-            </div>
-            <div className="rooms-container">
-                <h2 className="title">Salas</h2>
-                <input type="text" placeholder="busque por nome" />
-                <div className="room-list">
-                    <div className="list-header">
-                        <p>Nome da Sala</p>
-                        <p>Jogadores</p>
-                    </div>
-                    <Link to="/room" >
-                    <div className="room-item">
-                        <p>Sala geral 1</p>
-                        <p>32/50</p>
-                    </div>
-                    </Link>
+            <Background>
+                <div className="logo-container">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="268" height="76" viewBox="0 0 268 76">
+                        <text id="BIO" transform="translate(0 62)" fill="#fff" fontSize="57" fontFamily="SegoeUI, Segoe UI"><tspan x="0" y="0">BIO</tspan></text>
+                        <text id="DIVERSIDADE" transform="translate(98 41)" fill="#fff" fontSize="28" fontFamily="SegoeUI, Segoe UI"><tspan x="0" y="0">DIVERSIDADE</tspan></text>
+                        <text id="GENES" transform="translate(98 64)" fill="#fff" fontSize="11" fontFamily="SegoeUI, Segoe UI"><tspan x="0" y="0">GENES</tspan></text>
+                        <text id="ESPÉCIES" transform="translate(140 64)" fill="#fff" fontSize="11" fontFamily="SegoeUI, Segoe UI"><tspan x="0" y="0">ESPÉCIES</tspan></text>
+                        <text id="ECOSSISTEMAS" transform="translate(193 64)" fill="#fff" fontSize="11" fontFamily="SegoeUI, Segoe UI"><tspan x="0" y="0">ECOSSISTEMAS</tspan></text>
+                        <circle id="Elipse_1" data-name="Elipse 1" cx="1.5" cy="1.5" r="1.5" transform="translate(134 59)" fill="#fff"/>
+                        <circle id="Elipse_2" data-name="Elipse 2" cx="1.5" cy="1.5" r="1.5" transform="translate(187 59)" fill="#fff"/>
+                    </svg>
                 </div>
-            </div>
-            <div className="buttons-container">
-                <button>CRIAR SALA</button>
-                <button>RANKINGS</button>
-            </div>
-            <Link to="/" >
-                <h2>Sair</h2>
-            </Link>
+                <div className="rooms-container">
+                    <h2 className="title">Salas</h2>
+                    <input type="text" placeholder="busque por nome" />
+                    <div className="room-list">
+                        <div className="list-header">
+                            <p>Nome da Sala</p>
+                            <p>Jogadores</p>
+                        </div>
+                        <Link to="/room" >
+                        <div className="room-item">
+                            <p>Sala geral 1</p>
+                            <p>32/50</p>
+                        </div>
+                        </Link>
+                    </div>
+                    <button>ENTRAR NA SALA</button>
+                </div>
+                <div className="buttons-container">
+                    <button>CRIAR SALA</button>
+                    <button>RANKINGS</button>
+                </div>
+                <Link to="/" >
+                    <h2>Sair</h2>
+                </Link>
+            </Background>
         </div>
     )
 }
