23e9099a2c13153bb736b7808c1cb3958393a801
Carlos <carlosrocha@mecajun.com>
2020-11-16 11:06:13 -0300

    changing server ports

--- server/server.js
+++ server/server.js
@@ -3,16 +3,17 @@ const http = require('http')
 const socketio = require('socket.io')
 const path = require('path')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 const server = http.Server(app)
 const io = socketio(server)
-
+app.use(cors())
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, '..','build')))
+//app.use(express.static(path.resolve(__dirname, '..','build')))
 app.use(routes)
 
-server.listen(3000, ()=>{
-    console.log('> Server listening on port: 3000')
+server.listen(3333, ()=>{
+    console.log('> Server listening on port: 3333')
 })
