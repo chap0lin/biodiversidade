cd5ced88e40beea3100dce5a23e5ac9052cccb96
chap0lin <carlosrochacaar@gmail.com>
2023-08-24 11:37:42 -0300

    using https

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://adega.domqueshot.com:4000/'
+    baseURL: 'https://adega.domqueshot.com:4000/'
 })
 
 export default api;
